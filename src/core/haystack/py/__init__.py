29fb70f41c6cab8110a6b5e14b2ca64ce43880ab
trevadelman <trevor@xetobase.com>
2026-01-15 16:59:05 -0800

    Fix: negative duration unit selection in Number.make_duration
    When subtracting DateTimes resulting in negative durations, the magnitude
    comparison was using the raw duration instead of absolute value. This caused
    -2hr to display as -7200000ms because negative values are always less than
    positive thresholds.
    
    Fixed by using dur.abs_() for magnitude comparison.

--- src/core/haystack/py/__init__.py
+++ src/core/haystack/py/__init__.py
@@ -1,2 +1,34 @@
 # Hand-written haystack module for Python
 # Contains native implementations that mirror the ES/JavaScript versions
+
+# Patch Number._make_duration_body to fix negative duration unit selection
+def _patch_number():
+    from fan.haystack.Number import Number
+    from fan.sys.ObjUtil import ObjUtil
+    from fan.sys.Duration import Duration
+
+    def _make_duration_body_fixed(self, dur, unit=None):
+        """Fixed version that uses absolute value for magnitude comparison."""
+        if unit is None:
+            # Use absolute value for magnitude comparison (fixes negative duration unit selection)
+            abs_dur = dur.abs_()
+            if ObjUtil.compare_lt(abs_dur, Duration.make(1000000000)):
+                unit = Number.ms()
+            elif ObjUtil.compare_lt(abs_dur, Duration.make(60000000000)):
+                unit = Number.sec()
+            elif ObjUtil.compare_lt(abs_dur, Duration.make(3600000000000)):
+                unit = Number.mins()
+            elif ObjUtil.compare_lt(abs_dur, Duration.make(86400000000000)):
+                unit = Number.hr()
+            else:
+                unit = Number.day()
+
+        self._float_ = ((ObjUtil.to_float(ObjUtil.coerce(dur.ticks(), "sys::Num")) / 1.0E9) / unit.scale())
+        self._unit_ref = unit
+
+    Number._make_duration_body = _make_duration_body_fixed
+
+try:
+    _patch_number()
+except ImportError:
+    pass  # Number not yet loaded
